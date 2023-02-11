08acbbe09ce7df2324fdc52344a20f4f6bbbde2e
kittu <krutikpathak180@gmail.com>
2023-02-10 16:52:23 -0800

    Mean of Response Plots added

--- scripts/main.py
+++ scripts/main.py
@@ -1,8 +1,10 @@
+import os.path
 import sys
 
 import numpy as np
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
@@ -16,15 +18,17 @@ References:
 https://www.kaggle.com/code/sixteenpython/machine-learning-with-iris-dataset
 https://www.kaggle.com/code/tcvieira/simple-random-forest-iris-dataset/notebook
 https://scikit-learn.org/stable/modules/generated/sklearn.pipeline.Pipeline.html
+https://plotly.com/python/multiple-axes/
 
 """
 
 
-def ingest_data():
+def ingest_data(csv_url):
     data_source = pd.read_csv(
-        "../data/iris.data",
+        csv_url,
         names=["sepal_len", "sepal_wid", "petal_len", "petal_wid", "class"],
     )
+
     return data_source
 
 
@@ -40,6 +44,14 @@ def summary_statistic(arr, predictor):
 
 
 def create_plots(df):
+
+    # Checking if the plots folder is already available, if not creating the one
+    path = "../plots"
+    path_exist = os.path.exists(path)
+
+    if not path_exist:
+        os.mkdir(path)
+
     # Histogram Distribution Plot for Sepal Length
     fig = px.histogram(
         df,
@@ -50,6 +62,9 @@ def create_plots(df):
         title="Sepal Length Distribution for different species",
     )
     fig.show()
+    fig.write_html(
+        file=path + "/SepalLengthHistogramViolin.html", include_plotlyjs="cdn"
+    )
 
     # Histogram Distribution Plot for Sepal Width
     fig = px.histogram(
@@ -61,6 +76,9 @@ def create_plots(df):
         title="Sepal Width Distribution for different species",
     )
     fig.show()
+    fig.write_html(
+        file=path + "/SepalWidthHistogramViolin.html", include_plotlyjs="cdn"
+    )
 
     # Histogram Distribution Plot for Petal Length
     fig = px.histogram(
@@ -72,6 +90,9 @@ def create_plots(df):
         title="Petal Length Distribution for different species",
     )
     fig.show()
+    fig.write_html(
+        file=path + "/PetalLengthHistogramViolin.html", include_plotlyjs="cdn"
+    )
 
     # Histogram Distribution Plot for Petal Width
     fig = px.histogram(
@@ -83,6 +104,9 @@ def create_plots(df):
         title="Petal Width Distribution for different species",
     )
     fig.show()
+    fig.write_html(
+        file=path + "/PetalWidthHistogramViolin.html", include_plotlyjs="cdn"
+    )
 
     # Matrix Plot
     fig = px.scatter_matrix(
@@ -94,6 +118,7 @@ def create_plots(df):
         labels={col: col.replace("_", " ") for col in df.columns},
     )  # remove underscore
     fig.show()
+    fig.write_html(file=path + "/ScatterMatrix.html", include_plotlyjs="cdn")
 
 
 def machine_learning_pipelines(df):
@@ -126,10 +151,108 @@ def machine_learning_pipelines(df):
         print("{} Test Accuracy: {}".format(pipelines[i], model.score(X_test, y_test)))
 
 
+def make_mean_of_response_plots(predictor, species, iris):
+
+    predictor_map = {
+        "sepal_len": "Sepal Length",
+        "sepal_wid": "Sepal Width",
+        "petal_len": "Petal Length",
+        "petal_wid": "Petal Width",
+    }
+
+    species_map = {
+        "Iris-setosa": "Iris Setosa",
+        "Iris-versicolor": "Iris Versicolor",
+        "Iris-virginica": "Iris Virginica",
+    }
+
+    iris_predictor_species = iris[[predictor, "class"]]
+    a = np.array(iris_predictor_species[predictor])
+    population, bins = np.histogram(a, bins=10, range=(np.min(a), np.max(a)))
+    bins_mod = 0.5 * (bins[:-1] + bins[1:])
+
+    iris_predictor_subgroup = iris_predictor_species.loc[
+        iris_predictor_species["class"] == species
+    ]
+    b = np.array(iris_predictor_subgroup[predictor])
+    population_iris_class, _ = np.histogram(b, bins=bins)
+
+    population_response = population_iris_class / population
+    species_class_response_rate = len(iris.loc[iris["class"] == species]) / len(iris)
+    species_class_response_rate_arr = np.array(
+        [species_class_response_rate] * len(bins_mod)
+    )
+
+    fig = go.Figure(
+        data=go.Bar(
+            x=bins_mod,
+            y=population,
+            name=predictor_map[predictor],
+            marker=dict(color="blue"),
+        )
+    )
+
+    fig.add_trace(
+        go.Scatter(
+            x=bins_mod,
+            y=population_response,
+            yaxis="y2",
+            name="Response",
+            marker=dict(color="red"),
+        )
+    )
+
+    fig.add_trace(
+        go.Scatter(
+            x=bins_mod,
+            y=species_class_response_rate_arr,
+            yaxis="y2",
+            mode="lines",
+            name=species_map[species] + " Overall Response",
+        )
+    )
+
+    fig.update_layout(
+        title_text=species_map[species]
+        + " vs "
+        + predictor_map[predictor]
+        + " Mean of Response Rate Plot",
+        legend=dict(orientation="v"),
+        yaxis=dict(
+            title=dict(text="Frequency in Each Bin"),
+            side="left",
+            range=[0, 50],
+        ),
+        yaxis2=dict(
+            title=dict(text="Response"),
+            side="right",
+            range=[-0.1, 1.2],
+            overlaying="y",
+            tickmode="auto",
+        ),
+    )
+
+    # Set x-axis title
+    fig.update_xaxes(title_text="Predictor Bins")
+
+    fig.show()
+
+    # Saving the plots into an HTML file with dynamic path
+    path = "../plots"
+    path_exist = os.path.exists(path)
+
+    if not path_exist:
+        os.mkdir(path)
+
+    fig.write_html(
+        file=path + f"/{species_map[species]}vs{predictor_map[predictor]}_MOR.html",
+        include_plotlyjs="cdn",
+    )
+
+
 def main():
-    df = ingest_data()
-    print(df)
-    # printing summary statistic
+    csv_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+    df = ingest_data(csv_url)
     sepal_length_arr = np.array(df["sepal_len"])
     sepal_width_arr = np.array(df["sepal_wid"])
     petal_length_arr = np.array(df["petal_len"])
@@ -138,10 +261,21 @@ def main():
     summary_statistic(sepal_width_arr, "Sepal Width")
     summary_statistic(petal_length_arr, "Petal Length")
     summary_statistic(petal_width_arr, "Petal Width")
+
     # Plotting the graphs
     create_plots(df)
+
     # Building the machine learning pipelines
     machine_learning_pipelines(df)
+
+    # Create mean of response plots
+
+    predictors = ["sepal_len", "sepal_wid", "petal_len", "petal_wid"]
+    species = ["Iris-setosa", "Iris-versicolor", "Iris-virginica"]
+
+    for specimen in species:
+        for predictor in predictors:
+            make_mean_of_response_plots(predictor, specimen, df)
 
 
 if __name__ == "__main__":
