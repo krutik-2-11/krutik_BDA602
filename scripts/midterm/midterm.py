8a11a6739dd50e0720755e25454a169d707abb2a
kittu <krutikpathak180@gmail.com>
2023-04-01 13:57:41 -0700

    cat/cat and cont/cont correlation code added

--- scripts/midterm/midterm.py
+++ scripts/midterm/midterm.py
@@ -3,6 +3,7 @@ import sys
 
 import pandas as pd
 import statsmodels.api
+from correlation_metrics import CorrelationMetrics
 from data_loader import TestDatasets
 from mean_of_response import MeanOfResponse
 from predictor_vs_response_plots import PredictorVsResponsePlots
@@ -25,6 +26,21 @@ CONTINUOUS_PREDICTORS_CAPTION = "Continuous Predictors"
 IMPORTANCE = "importance"
 P_VALUE = "p_value"
 T_VALUE = "t_value"
+CONT_1 = "cont_1"
+CONT_2 = "cont_2"
+CAT_1 = "cat_1"
+CAT_2 = "cat_2"
+CORR = "corr"
+CONT_1_URL = "cont_1_url"
+CONT_2_URL = "cont_2_url"
+CAT_1_URL = "cat_1_url"
+CAT_2_URL = "cat_2_url"
+CORRELATION_PEARSON_CAPTION = "Correlation Pearson Table"
+CORRELATION_TSCHUPROW_CAPTION = "Correlation Tschuprow Table"
+CORRELATION_CRAMER_CAPTION = "Correlation Cramer Table"
+PEARSON = "Pearson's"
+TSCHUPROW = "Tschuprow"
+CRAMER = "Cramer"
 
 
 def load_dataset():
@@ -240,7 +256,204 @@ def create_logistic_regression(df, predictor, response):
     return summary_dict
 
 
-def correlation_matrix():
+def correlation_cont_cont_table(cm, df, cont_preds, df_mor_continuous):
+    """
+    Function to generate correlation matrix plot and table for cont/cont predictors
+    :param cm:
+    :param df:
+    :param cont_preds:
+    :param df_mor_continuous:
+    :return: df_corr_cont_cont_table
+    """
+    lst_corr_cont_cont_table = []
+    for pred_1 in cont_preds:
+        for pred_2 in cont_preds:
+            dict_corr_cont_cont_table = {
+                CONT_1: pred_1,
+                CONT_2: pred_2,
+                CORR: cm.cont_cont_correlation(df[pred_1], df[pred_2]),
+                CONT_1_URL: df_mor_continuous.loc[
+                    df_mor_continuous[PREDICTOR] == pred_1, PLOT_LINK_MOR
+                ].values[0],
+                CONT_2_URL: df_mor_continuous.loc[
+                    df_mor_continuous[PREDICTOR] == pred_2, PLOT_LINK_MOR
+                ].values[0],
+            }
+            lst_corr_cont_cont_table.append(dict_corr_cont_cont_table)
+
+    df_corr_cont_cont_table = pd.DataFrame(
+        lst_corr_cont_cont_table,
+        columns=[
+            CONT_1,
+            CONT_2,
+            CORR,
+            CONT_1_URL,
+            CONT_2_URL,
+        ],
+    )
+
+    df_corr_cont_cont_table = df_corr_cont_cont_table.sort_values(
+        by=[CORR], ascending=False
+    )
+    # plot the cont/cont correlation heatmap
+    cont_cont_corr_heatmap = cm.corr_heatmap_plots(
+        df_corr_cont_cont_table,
+        CONT_1,
+        CONTINUOUS_TYPE_PRED,
+        CONT_2,
+        CONTINUOUS_TYPE_PRED,
+        PEARSON,
+        CORR,
+    )
+
+    # Writing the matrix into HTML
+    with open("my_table.html", "a") as f:
+        f.write(cont_cont_corr_heatmap)
+
+    # Adding the condition to remove the self correlation values from the correlation table
+    df_corr_cont_cont_table = df_corr_cont_cont_table[
+        df_corr_cont_cont_table[CONT_1] != df_corr_cont_cont_table[CONT_2]
+    ]
+
+    return df_corr_cont_cont_table
+
+
+def correlation_cat_cat_table(cm, df, cat_preds, df_mor_categorical):
+    lst_corr_cat_cat_table_cramers_v = []
+    lst_corr_cat_cat_table_tschuprow = []
+    for pred_1 in cat_preds:
+        for pred_2 in cat_preds:
+            dict_corr_cat_cat_table_cramers_v = {
+                CAT_1: pred_1,
+                CAT_2: pred_2,
+                CORR: cm.cat_correlation(df[pred_1], df[pred_2]),
+                CAT_1_URL: df_mor_categorical.loc[
+                    df_mor_categorical[PREDICTOR] == pred_1, PLOT_LINK_MOR
+                ].values[0],
+                CAT_2_URL: df_mor_categorical.loc[
+                    df_mor_categorical[PREDICTOR] == pred_2, PLOT_LINK_MOR
+                ].values[0],
+            }
+            lst_corr_cat_cat_table_cramers_v.append(dict_corr_cat_cat_table_cramers_v)
+
+            dict_corr_cat_cat_table_tschuprow = {
+                CAT_1: pred_1,
+                CAT_2: pred_2,
+                CORR: cm.cat_correlation(df[pred_1], df[pred_2], tschuprow=True),
+                CAT_1_URL: df_mor_categorical.loc[
+                    df_mor_categorical[PREDICTOR] == pred_1, PLOT_LINK_MOR
+                ].values[0],
+                CAT_2_URL: df_mor_categorical.loc[
+                    df_mor_categorical[PREDICTOR] == pred_2, PLOT_LINK_MOR
+                ].values[0],
+            }
+            lst_corr_cat_cat_table_tschuprow.append(dict_corr_cat_cat_table_tschuprow)
+
+    df_corr_cat_cat_table_cramers_v = pd.DataFrame(
+        lst_corr_cat_cat_table_cramers_v,
+        columns=[
+            CAT_1,
+            CAT_2,
+            CORR,
+            CAT_1_URL,
+            CAT_2_URL,
+        ],
+    )
+
+    df_corr_cat_cat_table_tschuprow = pd.DataFrame(
+        lst_corr_cat_cat_table_tschuprow,
+        columns=[
+            CAT_1,
+            CAT_2,
+            CORR,
+            CAT_1_URL,
+            CAT_2_URL,
+        ],
+    )
+
+    # Sorting the values in correlation table in descending order
+    df_corr_cat_cat_table_cramers_v = df_corr_cat_cat_table_cramers_v.sort_values(
+        by=[CORR], ascending=False
+    )
+    df_corr_cat_cat_table_tschuprow = df_corr_cat_cat_table_tschuprow.sort_values(
+        by=[CORR], ascending=False
+    )
+
+    # plot the cont/cont correlation heatmap of cramer
+    cat_cat_corr_heatmap_cramers_v = cm.corr_heatmap_plots(
+        df_corr_cat_cat_table_cramers_v,
+        CAT_1,
+        CATEGORICAL_TYPE_PRED,
+        CAT_2,
+        CATEGORICAL_TYPE_PRED,
+        CRAMER,
+        CORR,
+    )
+
+    # plot the cont/cont correlation heatmap of tschuprow
+    cat_cat_corr_heatmap_tschuprow = cm.corr_heatmap_plots(
+        df_corr_cat_cat_table_tschuprow,
+        CAT_1,
+        CATEGORICAL_TYPE_PRED,
+        CAT_2,
+        CATEGORICAL_TYPE_PRED,
+        TSCHUPROW,
+        CORR,
+    )
+
+    # Writing the matrix into HTML
+    with open("my_table.html", "a") as f:
+        f.write(cat_cat_corr_heatmap_cramers_v)
+        f.write(cat_cat_corr_heatmap_tschuprow)
+
+    # Adding the condition to remove the self correlation values from the correlation table
+    df_corr_cat_cat_table_cramers_v = df_corr_cat_cat_table_cramers_v[
+        df_corr_cat_cat_table_cramers_v[CAT_1] != df_corr_cat_cat_table_cramers_v[CAT_2]
+    ]
+
+    df_corr_cat_cat_table_tschuprow = df_corr_cat_cat_table_tschuprow[
+        df_corr_cat_cat_table_tschuprow[CAT_1] != df_corr_cat_cat_table_tschuprow[CAT_2]
+    ]
+
+    return df_corr_cat_cat_table_cramers_v, df_corr_cat_cat_table_tschuprow
+
+
+def correlation_metrics(
+    df, predictors, response, df_mor_categorical, df_mor_continuous
+):
+    cm = CorrelationMetrics()
+    cat_preds, cont_preds = return_categorical_continuous_predictor_list(df, predictors)
+
+    # Getting the correlation coefficients of cont/cont predictors
+    df_corr_cont_cont_table = correlation_cont_cont_table(
+        cm, df, cont_preds, df_mor_continuous
+    )
+
+    # Save the cont_cont correlation table to HTML
+    save_dataframe_to_HTML(
+        df_corr_cont_cont_table, CONT_1_URL, CONT_2_URL, CORRELATION_PEARSON_CAPTION
+    )
+
+    # Getting the correlation coefficients of cat/cat predictors
+    (
+        df_corr_cat_cat_table_cramers_v,
+        df_corr_cat_cat_table_tschuprow,
+    ) = correlation_cat_cat_table(cm, df, cat_preds, df_mor_categorical)
+
+    # Save the cont_cont correlation table to HTML
+    save_dataframe_to_HTML(
+        df_corr_cat_cat_table_cramers_v,
+        CAT_1_URL,
+        CAT_2_URL,
+        CORRELATION_CRAMER_CAPTION,
+    )
+    save_dataframe_to_HTML(
+        df_corr_cat_cat_table_tschuprow,
+        CAT_1_URL,
+        CAT_2_URL,
+        CORRELATION_TSCHUPROW_CAPTION,
+    )
+
     return
 
 
@@ -457,6 +670,7 @@ def process_dataframes(dataset, df, predictors, response):
         on=None,
     )
     df_continuous_merged.reset_index(inplace=True)
+    df_mor_continuous.reset_index(inplace=True)
 
     # Merging the categorical predictors dataframes
     df_mor_categorical.set_index(PREDICTOR, inplace=True)
@@ -465,6 +679,7 @@ def process_dataframes(dataset, df, predictors, response):
         [df_resp_vs_pred_categorical], on=None
     )
     df_categorical_merged.reset_index(inplace=True)
+    df_mor_categorical.reset_index(inplace=True)
 
     save_dataframe_to_HTML(
         df_continuous_merged,
@@ -479,10 +694,9 @@ def process_dataframes(dataset, df, predictors, response):
         caption=CATEGORICAL_PREDICTORS_CAPTION,
     )
 
-    # save_dataframe_to_HTML(df_mor_categorical, PLOT_LINK_MOR, caption=CATEGORICAL_PREDICTORS_CAPTION)
-    # save_dataframe_to_HTML(df_mor_continuous, PLOT_LINK_MOR, caption=CONTINUOUS_PREDICTORS_CAPTION)
-    # save_dataframe_to_HTML(df_resp_vs_pred_categorical, PLOT_LINK, caption=CATEGORICAL_PREDICTORS_CAPTION)
-    # save_dataframe_to_HTML(df_resp_vs_pred_continuous, PLOT_LINK, caption=CONTINUOUS_PREDICTORS_CAPTION)
+    correlation_metrics(df, predictors, response, df_mor_categorical, df_mor_continuous)
+
+    return
 
 
 def main():
